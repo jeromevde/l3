a05412b2eb1355fb133e1ac801e604644c75fc74
Michel Schinz <michel@schi.nz>
2020-04-30 14:03:47 +0200

    Skeleton for assignment 6

--- compiler/src/l3/Main.scala
+++ compiler/src/l3/Main.scala
@@ -12,10 +12,26 @@ import CPSTreeChecker._   // Implicits required for CPS tree checking
 object Main {
   def main(args: Array[String]): Unit = {
     val backEnd: Tree => TerminalPhaseResult = (
+      // CL3Interpreter
       CL3ToCPSTranslator
-        andThen treePrinter("---------- After translation to CPS")
-        andThen treeChecker
-        andThen CPSInterpreterHigh
+        // andThen treePrinter("---------- After translation to CPS")
+        andThen CPSOptimizerHigh
+        // andThen treePrinter("---------- After high optimization")
+        // andThen CPSInterpreterHigh
+        andThen CPSValueRepresenter
+        andThen CPSHoister
+        // andThen treePrinter("---------- After value representation")
+        andThen CPSOptimizerLow
+        // andThen treePrinter("---------- After low optimization")
+        // andThen CPSInterpreterLow
+        andThen CPSConstantNamer
+        andThen CPSRegisterAllocator
+        andThen CPSToASMTranslator
+        // andThen seqPrinter("---------- After translation to assembly")
+        andThen ASMLabelResolver
+        // andThen ASMInterpreter
+        andThen ASMFileWriter(Option(System.getProperty("l3.out-asm-file"))
+                                .getOrElse("out.l3a"))
     )
 
     val basePath = Paths.get(".").toAbsolutePath
